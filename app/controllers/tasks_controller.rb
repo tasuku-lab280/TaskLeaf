8fdf958e63cc286cf64f15bb89b82780d7977cf0
Tasuku Kudo <loweng280@gmail.com>
2019-12-04 15:59:31 +0900

    finished ch3

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,45 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def new
     @task = Task.new
   end
 
-  def edit
+  def create
+    task = Task.new(task_params)
+    task.save!
+    flash[:success] = "タスク「#{task.name}」を登録しました。"
+    redirect_to tasks_url
   end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update!(task_params)
+    flash[:success] = "タスク「#{task.name}」を更新しました。"
+    redirect_to tasks_url
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    flash[:success] = "タスク「#{task.name}」を削除しました。"
+    redirect_to tasks_url
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description)
+  end
+
 end
